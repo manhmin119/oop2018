5255e46e8e45f33996d55fdc5973af77a0b116ba
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-09-21 10:30:37 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Administrator
+ */
 package week1;
 
 public class Student {
 
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+
     // TODO: khai báo các thuộc tính cho Student
-
     // TODO: khai báo các phương thức getter, setter cho Student
-
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22045";
+        this.email = "uet@vnu.edu.vn";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     /**
      * Constructor 2
+     *
      * @param n
      * @param sid
      * @param em
      */
     Student(String n, String sid, String em) {
         // TODO:
+        this.name = name;
+        this.id = id;
+        this.group = "INT22045";
+        this.email = email;
     }
+
+   
 
     /**
      * Constructor 3
+     *
      * @param s
      */
     Student(Student s) {
         // TODO:
+        this.name=s.name;
+        this.id=s.id;
+        this.group=s.group;
+        this.email=s.email;
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return name+" "id+" "+group+" "email;
+         
     }
 }
+
