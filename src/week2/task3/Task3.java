3928ab3231297f897f1872498c0832d9b077ce6c
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-09-28 16:03:58 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package week2.task3;
 
+/**
+ *
+ * @author CCNE
+ */
 public class Task3 {
+    public static void main(String[] args) {
+        
+    }
+}
+ class conmeo {
+    private String ten;
+    private int tuoi;
+    private String sothich;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getSothich() {
+        return sothich;
+    }
+
+    public void setSothich(String sothich) {
+        this.sothich = sothich;
+    }
+    public void an(){
+        System.out.println("ca");
+    } 
+    public void keu(){
+        System.out.println("meo meo");
+    }
+    public void longg(){
+             System.out.println("nhieu long");
+    }
+}
+class concho{
+    private String ten;
+    private int tuoi;
+    private String sothich;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getSothich() {
+        return sothich;
+    }
+
+    public void setSothich(String sothich) {
+        this.sothich = sothich;
+    }
+    public void an(){
+        System.out.println("cơm với xương");
+    }
+    public void keu(){
+        System.out.println("gâu gâu");
+    }
+    public void longg(){
+        System.out.println("vừa lông");
+    }
+  
+}
+class conchuot{
+    private String ten;
+    private int tuoi;
+    private String sothich;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getSothich() {
+        return sothich;
+    }
+
+    public void setSothich(String sothich) {
+        this.sothich = sothich;
+    }
+    public void an(){
+        System.out.println("ngũ cốc");
+    }
+    public void keu(){
+        System.out.println("chít chít");
+    }
+    public void longg(){
+        System.out.println("vừa lông");
+    }
 }
 
-//TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+
