fa03a2ba9ede33a02ecee743a4b3fa311051d590
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-09-28 10:14:11 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,31 @@
-package week2.task1;
+package week2;
 
+/**
+ *
+ * @author Administrator
+ */
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+    if(b==0) return a;
+    return gcd(b,a%b);
+// TODO: Tính ước chung lớn nhất của 2 số a, b
+     
+    }
+    
+    public static int fibonacci(int n) {
+        if(n==0) return 0;
+        else if(n==1) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
+        // TODO: Tìm số fibonacci ở vị trí n
+        
     }
 
-    public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
-    }
+   
+
+public static void main(String[] arg){
+
+    System.out.println(gcd(4,6));
+    System.out.println(fibonacci(4));
+}
 }
