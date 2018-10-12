071d72ae12038dfc53595e38f05b23b0f0717688
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-10-12 11:32:06 +0700

    Update package-info.java

--- src/week4/task1/package-info.java
+++ src/week4/task1/package-info.java
@@ -6,4 +6,103 @@
  * @author cuong
  * @version 0.1
  */
+
+/**
+ *
+ * @author Administrator
+ */
+
+/**
+ * Package bài làm cho Câu 1
+ * TODO: Khai báo các lớp (class) theo Câu 1 yêu cầu. 1 file chỉ khai báo 1 class
+ * TODO: Mô tả các quan hệ giữa các class và thêm thuộc tính, phương thức cho chúng (mỗi class ít nhất 2 phương thức, 2 thuộc tính)
+ * TODO: Viết comment đầy đủ cho class, phương thức. Xem hướng dẫn tại đây (http://dongdiemthuy.github.io/java/javadoc/2014/09/30/su-dung-javadoc-va-maven-de-tao-tai-lieu-du-an.html)
+ * @author cuong
+ * @version 0.1
+ */
 package week4.task1;
+/**
+ * 
+ * @author Administrator
+ */
+public class hoaqua{ 
+    //  TODO:khai bao thuoc tinh cho class
+    private String mausac;
+    private String huongvi;
+
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+
+    public String getHuongvi() {
+        return huongvi;
+    }
+
+    public void setHuongvi(String huongvi) {
+        this.huongvi = huongvi;
+    }
+    public void gia() {
+            System.out.println(" vua tien");
+}
+    public void chucnang(){
+        System.out.println("bồi bổ sức khỏe");
+}
+    
+}
+class quacam extends hoaqua{
+    private String huongvi;
+    private String mausac;
+
+    public String getHuongvi() {
+        return huongvi;
+    }
+
+    public void setHuongvi(String huongvi) {
+        this.huongvi = huongvi;
+    }
+
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+    public void chucnang(){
+        System.out.println(" thanh mát cơ thể");
+    }
+    public void gia(){
+        System.out.println("lúc thấp lúc cao");
+    }
+}
+class quatao extends hoaqua{
+    private String huongvi;
+    private String mausac;
+
+    public String getHuongvi() {
+        return huongvi;
+    }
+
+    public void setHuongvi(String huongvi) {
+        this.huongvi = huongvi;
+    }
+
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+    
+}
+class camcaophong extends quacam{
+    
+}
+class camsanh extends quacam{
+    
+}
