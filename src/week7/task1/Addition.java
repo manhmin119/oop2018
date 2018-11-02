7a70b5de58d315ff090e0a736e8d0820e49f852c
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 14:47:35 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,25 @@
-package week7.task1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
 
-public class Addition {
+/**
+ *
+ * @author CCNE
+ */
+public abstract class Addition extends BinaryExpression {
+    private Expression left;
+    private Expression right;
+    public  Addition(Expression left,Expression right){
+        this.left=left;
+        this.right=right;
+    }
+    public String toString(){
+        return "";
+    }
+    public int value(){
+        return 0;
+    }    
 }
