a31dbf168fa50be415f529b018244aa456381076
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 14:48:05 +0700

    Update Numeral.java

--- src/week7/task1/Numeral.java
+++ src/week7/task1/Numeral.java
@@ -1,4 +1,26 @@
-package week7.task1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
 
-public class Numeral {
+/**
+ *
+ * @author CCNE
+ */
+public class Numeral extends Expression{
+    private int value;
+    public Numeral(int value){
+        this.value=value;
+    }
+    public Numeral(){
+         this.value=0;
+    }
+    public String toString(){
+        return " ";
+    }
+    public int evaluate(){
+        return this.value;
+    }
 }
