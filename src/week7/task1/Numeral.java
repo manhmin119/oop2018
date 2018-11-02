b99a5f55153ab970b22d2298db8d1560e4abecab
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 23:49:45 +0700

    Update Numeral.java

--- src/week7/task1/Numeral.java
+++ src/week7/task1/Numeral.java
@@ -7,20 +7,21 @@ package week7;
 
 /**
  *
- * @author CCNE
+ * @author Administrator
  */
 public class Numeral extends Expression{
-    private int value;
-    public Numeral(int value){
-        this.value=value;
-    }
-    public Numeral(){
-         this.value=0;
-    }
-    public String toString(){
-        return " ";
-    }
-    public int evaluate(){
-        return this.value;
-    }
+  private int value;
+	public Numeral(int value) {
+		this.value=value;
+	}
+	public Numeral() {
+		this.value=0;
+	}
+	public int evaluate() {
+		return this.value;
+	}
+	public String toString() {
+		return  " "+this.value;
+	}
+	
 }
