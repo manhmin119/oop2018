4cb59d8c47c76ceec2f8345da985609751aab0b6
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 14:46:49 +0700

    Update Square.java

--- src/week7/task1/Square.java
+++ src/week7/task1/Square.java
@@ -1,4 +1,23 @@
-package week7.task1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
 
-public class Square {
+/**
+ *
+ * @author CCNE
+ */
+public class Square extends Expression {
+    public Expression expression;
+    public Square(Expression expression){
+       this.expression= expression;
+    }
+     public String toString(){
+        return "";
+    }
+    public int evaluate(){
+        return this.expression.evaluate();
+    }
 }
