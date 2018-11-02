87c788637efe66b9f1a953047ffac635915322a6
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 14:47:14 +0700

    Update BinaryExpression.java

--- src/week7/task1/BinaryExpression.java
+++ src/week7/task1/BinaryExpression.java
@@ -1,4 +1,17 @@
-package week7.task1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
 
-public class BinaryExpression {
+/**
+ *
+ * @author CCNE
+ */
+public abstract class BinaryExpression extends Expression {
+       public abstract Expression left();
+       public abstract Expression right();
+       public abstract String toString();
+       public abstract int evaluate();
 }
