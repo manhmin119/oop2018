fcde1a43c055e7e4a5ad16484083b94a83f10afa
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-02 14:45:35 +0700

    Update Expression.java

--- src/week7/task1/Expression.java
+++ src/week7/task1/Expression.java
@@ -1,4 +1,21 @@
-package week7.task1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
 
+/**
+ *
+ * @author CCNE
+ */
 public class Expression {
+    public String toString(){
+        return " ";
+    }
+    public int evaluate(){
+        return 0;
+    }
+    
 }
+
