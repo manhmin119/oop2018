d51979fc55dfdc3cd2366b45ae0ae4bea1c2f61c
manhmin119 <43264991+manhmin119@users.noreply.github.com>
2018-11-09 14:11:12 +0700

    Update Utils.java

--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 /**
  *
  * @author Administrator
@@ -32,12 +33,27 @@ public class Utils {
 }
     public static  void writeContentToFile(String path) {
         try{
-        FileWriter fw=new FileWriter(new File(path));
-        fw.write("ghi dữ liệu");
+        FileWriter fw=new FileWriter(new File(path), true);
+            Scanner sc= new Scanner(System.in);
+            String data= sc.nextLine();
+        fw.write(data);
         fw.close();
         }catch(IOException e){
     e.printStackTrace();
 }
 }
+    public static File findFileByName(String folderPath,String fileName){
+        String a= folderPath+"\\"+ fileName;
+        File f=new File(a);
+        if(f.exists()){
+            return f;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //writeContentToFile("C:\\New folder\\manh.txt");
+        System.out.println(findFileByName("C:\\New folder","thuy.txt"));
+    }
 }
 
